4ed899a20336798bb777cb81525595d840cb7923
Chelmus Marian <chelmus.marian@gmail.com>
2018-11-12 19:56:41 +0200

    Initial deploy: cfgIP -> config static IP for Centos and Ubuntu with cloud init

---
+++ cfgIp.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+#VARS
+os=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }' | sed 's/"//' | sed 's/["]//')
+
+interface=$(ip addr show | awk '/inet.*brd/{print $NF}' | head -n 1)
+
+
+netCentos="/etc/sysconfig/network-scripts/ifcfg-$interface"
+netUbuntu="/etc/network/interfaces"
+
+maskCentos=$(ip -f inet a show $interface | grep inet | awk '{ print $2 }' | rev | cut -d / -f1 | rev)
+maskUbuntu=$(ifconfig $interface | sed -rn '2s/ .*:(.*)$/\1/p')
+
+gateway=$(ip r | grep default | awk '{print $3}')
+
+
+defIP=$(ip -f inet a show $interface | grep inet | awk '{ print $2 }' | cut -d / -f1)
+
+# cloud init location
+cloudInitDisable="/etc/cloud/cloud.cfg.d/99-disable-network-config.cfg"
+cloudInit="/etc/network/interfaces.d/50-cloud-init.cfg"
+# configure static IP functions
+
+centos () {
+        echo "---------------------------------------------------------------"
+	echo "Configuring the interface for $OS"
+	if [ ! -f $netCentos ]
+	then
+		echo "Network configuration file not found... exiting"
+		exit 1
+	else
+		echo -e "Network configuration file located at: "'\033[1m' $netCentos'\033[0m'
+		if grep -q dhcp $netCentos
+		then
+			sed -i "s/dhcp/static/g" $netCentos
+			sed -i "s/.*BOOTPROTO.*/&\nIPADDR=\"$defIP\"/" $netCentos
+			sed -i "s/.*IPADDR.*/&\nPREFIX=\"$maskCentos\"/" $netCentos
+			sed -i "s/.*PREFIX.*/&\nGATEWAY=\"$gateway\"/" $netCentos
+			sed -i "s/.*GATEWAY.*/&\nDNS1=\"8.8.8.8\"/" $netCentos
+			sed -i "s/.*GATEWAY.*/&\nDNS2=\"8.8.4.4\"/" $netCentos
+			echo "Interface was configured:"
+			cat $netCentos
+		fi
+	fi
+}
+
+
+ubuntu () {
+
+        echo "---------------------------------------------------------------"
+        echo "Configuring the interface for $OS"
+        if [ ! -f $netUbuntu ]
+        then
+                echo "Configuration file not found... Exiting"
+                exit 1
+        else
+                if [ -f $cloudInit ] && [ ! -f $cloudInitDisable ]
+		then
+		        echo "Cloud init is enabled. Disabling..."
+	       	        touch  $cloudInitDisable
+		        echo "network: {config: disabled}" > $cloudInitDisable
+			echo -e "Network configuration file located at: " '\033[1m' $netUbuntu'\033[0m'
+                        echo "auto $interface" >> $netUbuntu
+			echo "iface $interface inet static" >> $netUbuntu
+			echo "        address $defIP" >> $netUbuntu
+                        echo "        netmask $maskUbuntu" >> $netUbuntu
+                        echo "        gateway $gateway" >> $netUbuntu
+                        echo "        dns-nameservers 8.8.8.8 8.8.4.4" >> $netUbuntu
+                        echo "Interface was configured: "
+                        cat $netUbuntu
+                else
+                        echo "Interface was already configured"
+                        cat $netUbuntu
+                fi
+        fi
+
+}
+
+echo "select the OS  1) Cent0S 2) UBUNTU"
+
+read n
+case $n in
+    1) centos;;
+    2) ubuntu;;
+    *) invalid option;;
+esac
+
+
