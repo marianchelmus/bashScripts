8b3bdf30ed11031ef026afeceeb45c0069e377db
Chelmus Marian <chelmus.marian@gmail.com>
2018-11-14 07:34:31 +0200

    updat cfgIP.sh with function to add multiple IPs on centos

--- cfgIp.sh
+++ cfgIp.sh
@@ -81,6 +81,64 @@ ubuntu () {
 
 }
 
+
+# ifconfig is not installed on minimal and we need it in order to get the netmask
+requirements () {
+ifconfig > /dev/null 2>&1
+if [ $? = 127 ]; then
+        yum install -qy net-tools
+elif [ $? = 0 ]; then
+        echo "Net-tools is already installed"
+        break
+fi
+}
+
+#check if the IPs entered are in a valid format and saves them to a /tmp/iplist which will be later
+removed
+validateIps () {
+
+ipValidator='^(([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\.){3}([1-9]?[0-9]|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$'
+while true
+do
+#       touch /tmp/iplist
+        echo -n "Insert IP [Type 0 to exit]: "
+        read IP
+        if [[ $IP =~ $ipValidator ]]; then
+                echo "Good"
+                echo $IP >> /tmp/iplist
+        elif [[ $IP = 0 ]]; then
+                echo "exiting function"
+                break
+        else
+                echo "$IP is not in supported format [Type 0 to exit] "
+        fi
+done
+}
+
+addIpsCentos () {
+
+n="0"
+numberofIPs=$(wc -l /tmp/iplist | awk '{print $1}')
+interface=$(ip addr show | awk '/inet.*brd/{print $NF}' | head -n 1)
+netmask=$(ifconfig $interface | grep netmask | awk '{print$4}')
+netCentos="/etc/sysconfig/network-scripts/ifcfg-$interface"
+while [ $n -lt $numberofIPs ]
+do
+        touch $netCentos:$n
+        for IP in `cat /tmp/iplist`;do
+                echo "Configuring $IP on $netCentos:$n"
+                echo "DEVICE=\"$interface:$n\"" >> $netCentos:$n
+                echo "IPADDR=\"$IP\"" >> $netCentos:$n
+                echo "NETMASK=\"$netmask\"" >> $netCentos:$n
+                echo "ONBOOT=\"yes\"" >> $netCentos:$n
+                n=$(( $n + 1 ))
+        done
+done
+}
+
+
+
+
 echo "select the OS  1) Cent0S 2) UBUNTU"
 
 read n
@@ -90,4 +148,20 @@ case $n in
     *) invalid option;;
 esac
 
-
+echo "Do you want to add more IPs? 1) Yes 2) No"
+read m
+case $m in
+	1) requirements
+	   validateIps
+	   if [ os == centos ]]then;
+		   addIpsCentos
+	   elif [ os == ubuntu ]then;
+		   addIpsUbuntu
+	   else
+		   echo "OS can not be detected. Exiting"
+		   exit 1
+	   fi
+	2) "Scripts finished with no errors. Exiting"
+	    break;
+	*) invalid option;;
+esac
